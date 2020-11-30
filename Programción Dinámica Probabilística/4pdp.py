2f785b4025fa370401dc12d7e407d5bedaaafc25
David Armando Rodríguez Varón <52163802+Darvaron@users.noreply.github.com>
2020-11-30 11:39:39 -0500

    4 corregido
    Falta arreglar que muestre los valores de x

--- Programción Dinámica Probabilística/4pdp.py
+++ Programción Dinámica Probabilística/4pdp.py
@@ -1,17 +1,13 @@
 def prob(n, xn):
     '''
-
     Retorna la probabilidad del componente dado
     Args:
         n: número de la etapa
         xn: cantidad de unidades paralelas
-
     Returns: probabilidad de que el componente n funcione con x unidades paralelas
-
     '''
 
     n -= 1
-    xn -= 1
 
     probs = [[0.5, 0.42, 0.36, 0.25],
              [0.6, 0.51, 0.41, 0.36],
@@ -22,36 +18,28 @@ def prob(n, xn):
 
 def cost(n, xn):
     '''
-
     Retorna el precio asociado a instalar xn unidades del componente n
     Args:
         n: número de la etapa
         xn: cantidad de unidades paralelas
-
     Returns: precio de instalar xn unidades del componente n
-
     '''
 
     n -= 1
-    xn -= 1
 
-    price = [[100, 200, 300],
-             [200, 400, 500],
-             [100, 300, 400],
-             [200, 300, 400]]
+    price = [[0, 1, 2, 3],
+             [0, 1, 2, 3],
+             [0, 1, 2, 3]]
 
     return price[n][xn]
 
 
 def step_prices(n):
     '''
-
     Retorna el dinero disponible asociado a cada etapa
     Args:
         n: número de la etapa
-
     Returns: cantidades de dinero disponible para la etapa n
-
     '''
 
     n -= 1
@@ -63,33 +51,13 @@ def step_prices(n):
 
     return step_prices[n]
 
-def min_price(n):
-    '''
-
-    Retorna el dinero minimo que debe ser guardado para comprar el minimo de componentes restantes
-    Args:
-        n: etapa anterior
-
-    Returns: dinero minimo que necesita ser guardado
-
-    '''
-
-    n -= 1
-
-    min_price = [600, 500, 300, 200, 0]
-
-    return min_price[n]
-
 def step(n, last_step, cache):
     '''
-
     Args:
         n: número de la etapa
         last_step: soluciones optimas para la etapa anterior
         cache: resultados de todos los niveles
-
     Returns: tupla que contiene la solución optima
-
     '''
 
     fn = []
@@ -99,7 +67,7 @@ def step(n, last_step, cache):
 
     for s in sn: # Calcula los valores de la tabla
         fn_sn = []
-        for i in range(1, 4):
+        for i in range(0, 4):
             if s - cost(n, i) >= sn_op[0]: # Si alcanza el dinero para comprar i componentes tipo n
                 j = 0
                 for j, s_last in enumerate(sn_op):  # n -- n + 1
@@ -116,9 +84,9 @@ def step(n, last_step, cache):
 
     for (s, fs) in fn: #Elije los valores optimos para cada valor de s
         x_op = 0
-        value = -10000
+        value = 10000
         for i, v in enumerate(fs):
-            if v > value:
+            if v < value:
                 x_op = i + 1
                 value = v
         xop_current.append(x_op)
@@ -135,13 +103,10 @@ def step(n, last_step, cache):
 
 def get_sol(sol):
     '''
-
     Obtiene la solución más optima y la muestra
     Args:
         sol: cache que contiene las soluciones optimas de cada nivel
-
     Returns: None
-
     '''
 
     budget = 1000
@@ -178,9 +143,9 @@ def get_sol(sol):
 def main():
     n = 3
 
-    sn_3 = [4, 4, 4, 4, 4]
-    v_3 = [1, 1, 1, 1, 1]
-    x_3 = [0, 0, 0, 0, 0]
+    sn_3 = [0, 1, 2, 3]
+    v_3 = [1, 1, 1, 1]
+    x_3 = [0, 0, 0, 0]
     step_3 = (sn_3, v_3, x_3)
 
     sol = step(n, step_3, [])
