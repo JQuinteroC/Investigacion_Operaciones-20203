b4609b544db223afd4d8f4dce8f46369cc23895a
JQuinteroC <jlqc1772@gmail.com>
2020-12-16 01:21:18 -0500

    Sn y Xn subidas

--- Programción Dinámica Probabilística/pdp_program/Controller.py
+++ Programción Dinámica Probabilística/pdp_program/Controller.py
@@ -8,8 +8,12 @@ class Controlador():
         self.app.btnCalcular['command'] = self.calcular
         self.app.btnProba['command'] = self.proba
         self.app.btnCost['command'] = self.cost
+        self.app.btnVetp['command'] = self.etapa
+        self.app.btnVvar['command'] = self.var
         self.probs = []
         self.costs = []
+        self.sn = []
+        self.xn = []
         self.app.mainloop()
 
         # Metodo para iniciar el calculo
@@ -23,9 +27,10 @@ class Controlador():
                 if j == 0 and i == 0:
                     tem.append(int(self.app.venPro.children['!entry'].get())/100)
                 else:
-                    tem.append(int(self.app.venPro.children['!entry'+str(j+i+1)].get())/100)
+                    tem.append(int(self.app.venPro.children['!entry'+str(j+(i*n)+1)].get())/100)
             self.probs.append(tem)
         self.app.venPro.destroy()
+        print(self.probs)
     
     def aceCost(self):
         m = int(self.app.txtCostM.get())
@@ -36,9 +41,42 @@ class Controlador():
                 if j == 0 and i == 0:
                     tem.append(int(self.app.venCosto.children['!entry'].get()))
                 else:
-                    tem.append(int(self.app.venCosto.children['!entry'+str(j+i+1)].get()))
+                    tem.append(int(self.app.venCosto.children['!entry'+str(j+(i*n)+1)].get()))
             self.costs.append(tem)
         self.app.venCosto.destroy()
+        print(self.costs)
+    
+    def aceEtapa(self):
+        m = int(self.app.txtVetpM.get())
+        n = int(self.app.txtVetpN.get())
+        for i in range(m):
+            tem = []
+            for j in range(n):
+                if j == 0 and i == 0:
+                    tem.append(int(self.app.venEtapa.children['!entry'].get()))
+                else:
+                    tem.append(int(self.app.venEtapa.children['!entry'+str(j+(i*n)+1)].get()))
+            for j in range(n-1):
+                if tem[j+1] == 0:
+                    tem = tem[0:j+1]
+                    break
+            self.sn.append(tem)
+        self.app.venEtapa.destroy()
+        print(self.sn)
+    
+    def aceVar(self):
+        m = int(self.app.txtVvarM.get())
+        for i in range(1):
+            tem = []
+            for j in range(m):
+                if j == 0 and i == 0:
+                    tem.append(int(self.app.venVar.children['!entry'].get()))
+                else:
+                    tem.append(int(self.app.venVar.children['!entry'+str(j+i+1)].get()))
+            for i in range(m):
+                self.xn.append(tem[i])
+        self.app.venVar.destroy()
+        print(self.xn)
     
     def proba(self):
         m = int(self.app.txtProbM.get())
@@ -51,6 +89,17 @@ class Controlador():
         n = int(self.app.txtCostN.get())
         self.app.Costo(m,n)
         self.app.venCosto.children['!button']['command'] = self.aceCost
+    
+    def etapa(self):
+        m = int(self.app.txtVetpM.get())
+        n = int(self.app.txtVetpN.get())
+        self.app.Etapa(m,n)
+        self.app.venEtapa.children['!button']['command'] = self.aceEtapa
+    
+    def var(self):
+        m = int(self.app.txtVvarM.get())
+        self.app.Variable(m)
+        self.app.venVar.children['!button']['command'] = self.aceVar
 
     def calcular(self):
         respuesta = ""
@@ -116,7 +165,7 @@ class Controlador():
             v_last.append(1)
             x_last.append(0)
 
-        prog = pdp_gen(self.probs, self.costs, sn, xn, typev)
+        prog = pdp_gen(self.probs, self.costs, self.sn, self.xn, typev)
         prog.run(n, sn_last, v_last, x_last, budget)
         respuesta += prog.res
         self.app.respuesta.insert("1.0", respuesta)
--- Programción Dinámica Probabilística/pdp_program/Gui.py
+++ Programción Dinámica Probabilística/pdp_program/Gui.py
@@ -10,29 +10,25 @@ class Aplicacion(ttk.Frame):
         super().__init__(ventana)
         ventana.title('Programación dinamica probabilistica')
         
-        ventana.configure(width = 610, height = 500)
+        ventana.configure(width = 610, height = 440)
         self.place(relwidth = 1, relheight = 1)
        # self.root.resizable(width=False, height=False)
 
         # Declaración de los componentes
         self.iteraciones = ttk.Label(self, text = "N° de iteraciones:", width = 20, font = ("","14"))
         self.iteraciones.place(x = 20, y = 20)
-
         self.txtIteraciones = ttk.Entry(self, width = 2,font = ("","14"))
         self.txtIteraciones.place(x = 175, y = 20)
 
+        self.dec = ttk.Label(self, text="1 para maximizar, 2 para minizar:", font=("", "14"))
+        self.dec.place(x=280, y=20)
+        self.txtDec = ttk.Entry(self, width=2, font=("", "14"))
+        self.txtDec.place(x=560, y=20)
 
         self.anterior = ttk.Label(self, text = f"Restricción:", font = ("", "14"))
-        self.anterior.place(x = 225, y = 20)
-
+        self.anterior.place(x = 20, y = 60)
         self.txtAnterior = ttk.Entry(self, width = 10,font = ("","14"))
-        self.txtAnterior.place(x = 470, y = 20)
-
-        self.dec = ttk.Label(self, text="1 para maximizar, 2 para minizar:", font=("", "14"))
-        self.dec.place(x=20, y=60)
-
-        self.txtDec = ttk.Entry(self, width=1, font=("", "14"))
-        self.txtDec.place(x=300, y=60)
+        self.txtAnterior.place(x = 130, y = 60)
 
         self.prob = ttk.Label(self, text="Probabilidades MxN:", font=("", "14"))
         self.prob.place(x=20, y=100)
@@ -52,11 +48,27 @@ class Aplicacion(ttk.Frame):
         self.btnCost = ttk.Button(self, text = "Costos")
         self.btnCost.place(x=205, y=140)
 
+        self.vetp = ttk.Label(self, text="Valores etapa MxN:", font=("", "14"))
+        self.vetp.place(x=20, y=180)
+        self.txtVetpM = ttk.Entry(self, width=2,font=("", "14"))
+        self.txtVetpM.place(x=190, y=180)
+        self.txtVetpN = ttk.Entry(self, width=2,font=("", "14"))
+        self.txtVetpN.place(x=220, y=180)
+        self.btnVetp = ttk.Button(self, text = "Etapas")
+        self.btnVetp.place(x=260, y=180)
+
+        self.vvar = ttk.Label(self, text="Valores variable 1xM:", font=("", "14"))
+        self.vvar.place(x=20, y=220)
+        self.txtVvarM = ttk.Entry(self, width=2,font=("", "14"))
+        self.txtVvarM.place(x=210, y=220)
+        self.btnVvar = ttk.Button(self, text = "Variables")
+        self.btnVvar.place(x=250, y=220)
+
         self.respuesta = ScrolledText(self, width = 70, height = 8)
-        self.respuesta.place(x = 20, y = 250)
+        self.respuesta.place(x = 20, y = 260)
         
         self.btnCalcular = ttk.Button(self, text ='Calcular')
-        self.btnCalcular.place(x = 510, y = 420)
+        self.btnCalcular.place(x = 510, y = 400)
     
     def Prob(self, m, n):
         self.venPro = tk.Tk()
@@ -78,16 +90,22 @@ class Aplicacion(ttk.Frame):
         btnAceptarCost = ttk.Button(self.venCosto, text ='Aceptar')
         btnAceptarCost.grid(padx=m+1, pady=1, row=m, column=0)
     
-    def prob(self, m, n):
-        self.venPro = tk.Tk()
+    def Etapa(self, m, n):
+        self.venEtapa = tk.Tk()
         for r in range(0, m):
             for c in range(0, n):
-                cell = ttk.Entry(self.venPro, width=8)
+                cell = ttk.Entry(self.venEtapa, width=8)
                 cell.grid(padx=m, pady=n, row=r, column=c)
-        self.venPro.title("Probabilidades")
-        btnAceptarPro = ttk.Button(self.venPro, text ='Aceptar')
-        btnAceptarPro.grid(padx=m+1, pady=1, row=m, column=0)
-        
-        
-       
-
+        self.venEtapa.title("Etapa")
+        btnAceptarEtp = ttk.Button(self.venEtapa, text ='Aceptar')
+        btnAceptarEtp.grid(padx=m+1, pady=1, row=m, column=0)
+    
+    def Variable(self, m):
+        self.venVar = tk.Tk()
+        for r in range(0, 1):
+            for c in range(0, m):
+                cell = ttk.Entry(self.venVar, width=8)
+                cell.grid(padx=1, pady=m, row=r, column=c)
+        self.venVar.title("Variable")
+        btnAceptarCost = ttk.Button(self.venVar, text ='Aceptar')
+        btnAceptarCost.grid(padx=2, pady=1, row=1, column=0)
