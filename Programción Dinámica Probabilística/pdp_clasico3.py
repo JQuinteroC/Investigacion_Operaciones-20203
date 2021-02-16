38b7e5eaa073084ecb5e3b06d6bd4b7c7c66f5dd
JQuinteroC <jlqc1772@gmail.com>
2021-02-15 20:32:04 -0500

    Ejercicio clásico y de aplicación 3

---
+++ Programción Dinámica Probabilística/pdp_clasico3.py
@@ -0,0 +1,68 @@
+def funcion_lote(x, funcion_recursiva):
+    """Calcula la función funcion_recursiva según X
+
+    Args:
+        x (int): variable de desición
+        funcion_recursiva (none): función a operar
+
+    Returns:
+        int: valor de la funcion funcion_recursiva evaluada en x
+    """
+    if x == 0:
+        return x + funcion_recursiva * (0.2**x)
+
+    return 7 * x + funcion_recursiva * (0.2**x)
+
+
+def min_valor(funcion_recursiva):
+    """Evalua la funcion_recursiva en busqueda del menor valor posible
+
+    Args:
+        funcion_recursiva (none): función recursiva de la etapa
+
+    Returns:
+        list: menor valor de la función y valor de X
+    """
+    xn = []
+
+    xn.append(funcion_lote(0, funcion_recursiva))
+
+    # Se pone en dos condiciones para revisar el siguiente y el siguiente a este, pues se puede dar el caso...tal vez se podria optimizar
+    while xn[len(xn) - 1] >= funcion_lote(len(xn), funcion_recursiva) or xn[len(xn) - 1] >= funcion_lote(len(xn) + 1, funcion_recursiva):
+        xn.append(funcion_lote(len(xn), funcion_recursiva))
+
+    return min(xn), xn.index(min(xn))
+
+
+def iteracion(numero_iteraciones, valor_inial):
+    """Realiza las numero_iteraciones con la función valor_inicial
+
+    Args:
+        numero_iteraciones (int): Número de iteraciones a realizar
+        valor_inial (float): Valor de la restricción inicial
+
+    Returns:
+        List[list]: Una lista de listas con la información del menor valor y valor de X por iteración
+    """
+    sol_iteracion = []
+
+    sol_iteracion.append(min_valor(valor_inial))
+
+    for i in range(numero_iteraciones - 1):
+        sol_iteracion.append(min_valor(sol_iteracion[i][0]))
+
+    sol_iteracion.reverse()
+
+    return sol_iteracion
+
+
+if __name__ == "__main__":
+    # Valores del problema
+    numero_iteraciones = 3
+    funcion_inial = 21
+
+    solucion = iteracion(numero_iteraciones, funcion_inial)
+    solucion = [(14.25486, 10), (13.39157, 8), (13.24673, 8)]
+    for i in range(len(solucion)):
+        print(
+            f'En el dia de producción {i+1}, la cantidad optima de lotes de vacunas a producir es de {solucion[i][1]} con un con un costo de {solucion[i][0]*100}$')
